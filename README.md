6a5539f907ccbd29411f8f3a2b6d82379912164a
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2025-08-09 23:29:45 +0530

    chore: Migrated older CRA to React with Parcel

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-<h1 align = "center"> FeastFly </h1>
+<h1 align = "center"> NoteStack </h1>
 
 ## Technologies 🛠️
+- HTML, CSS, JavaScript
+- React.js
+- Parcel
 
 ## Installation 💻
 
-## Screenshots 📸
+- Clone the Repository
+     
+```bash
+    https://github.com/Sudeep-Poojary/NoteStack.git
+```
+
+## Screenshot 📸
+
+<div align="center">
+
+![NoteStack](./src/images/NoteStack.png "NoteStack")
+
+</div>
---
+++ src/components/CreateArea.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const CreateArea = (props) => {
+  const [note, setNote] = useState({ title: "", conten: "" });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  };
+
+  const submitNote = (event) => {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form className="create-note">
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={submitNote}>➕</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateArea;
